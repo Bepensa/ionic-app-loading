fff2213de77d98a988fab435e496c85eb4457145
rbcetinag <rbcetinag@bepensa.com>
2022-09-28 12:09:39 -0500

    Loader added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,33 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    private http: HttpClient,
+    private loadingController: LoadingController
+  ) {}
+
+  showLoader() {
+    this.loadingController.create({
+      message: 'Loading...'
+    }).then((response) => {
+      response.present();
+      this.getData().subscribe((data: any) => {
+        console.log(data.mensaje);
+        this.dismissLoader();
+      });
+    });
+  }
+
+  dismissLoader() {
+    this.loadingController.dismiss().then((response) => {
+      console.log('Loader closed!', response);
+    }).catch((err) => {
+      console.log('Error occured : ', err);
+    });
+  }
+
+  getData() {
+    return this.http.get('https://bconnectapitest.azurewebsites.net/Consumidores/HorarioVisitaRuta?numeroCelular=9991656778&latitud=21.028215&longitud=-89.690359');
+  }
 
 }
